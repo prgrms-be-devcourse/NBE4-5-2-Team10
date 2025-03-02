160a98a016b234d40bf596b2625c62a405701db3
kces0822 <kces0822@gmail.com>
2025-03-02 15:53:37 +0900

    :wrench: chore: jdk 17로 변경, spring boot 3.2.4로 변경, 테스트용 HomeController 작성

---
+++ backend/src/main/java/com/example/travelPlan/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.travelPlan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/main")
+    @ResponseBody
+    public String main(){
+        return "hello world";
+    }
+}
