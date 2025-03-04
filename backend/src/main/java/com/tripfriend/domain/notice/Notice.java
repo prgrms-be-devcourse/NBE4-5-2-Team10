6eb8992b7abebc292119956b99e7e8c265bf6cca
hoechanj <154239980+hoechanj@users.noreply.github.com>
2025-03-04 15:49:59 +0900

    ✨feat(16): notice entity 작성 (#18)
    * 🔧chore : 빌드 문제 수정
    
    - com.example.travelPlan에서 com.tripfriend로 수정
    
    * [feat]notice entity 작성
    
    ---------
    
    Co-authored-by: GODAOS <jmskkr@gmail.com>

---
+++ backend/src/main/java/com/tripfriend/domain/notice/Notice.java
@@ -0,0 +1,45 @@
+package com.tripfriend.domain.notice;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
