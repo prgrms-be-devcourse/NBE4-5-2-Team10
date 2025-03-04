0cd15fa21f4f14600622edd01fec486fac5b595d
Shin Youn Ho <115200471+messiteacher@users.noreply.github.com>
2025-03-04 16:10:44 +0900

    ✨ Feat: 회원 CRUD 기능 구현 (#19)

--- backend/src/main/java/com/tripfriend/TravelPlanApplication.java
+++ backend/src/main/java/com/tripfriend/TravelPlanApplication.java
@@ -2,8 +2,10 @@ package com.tripfriend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TravelPlanApplication {
 
 	public static void main(String[] args) {
