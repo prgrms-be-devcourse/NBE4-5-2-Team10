4a0f709eb934813e89bc5b3f7166c4edcfab61e7
kyungrae kim <15260002+GODAOS@users.noreply.github.com>
2025-03-05 11:21:41 +0900

    여행지 CRUD 구현 (#27)
    * ♻️refactor : TripSchedule 컬럼 타입 변경
    
    - 시작일, 종료일 타입값 LocalDate로 변경
    
    * ♻️refactor : Place 수정
    
    - 카테고리 columDefinition 오류 수정
    
    * ✨feat : Place 등록
    
    - cityName, placeName, description, category 입력 후 장소 등록
    
    * ✨feat : Place 전체 조회, 단건 조회
    
    - 여행지 전체 조회와 단건 조회 가능
    
    * ✨feat : Place 삭제, 수정 기능
    
    - 여행장소를 id로 구분해서 삭제와 수정이 가능

---
+++ backend/src/main/java/com/tripfriend/domain/place/place/dto/PlaceResDto.java
@@ -0,0 +1,24 @@
+package com.tripfriend.domain.place.place.dto;
+
+import com.tripfriend.domain.place.place.entity.Category;
+import com.tripfriend.domain.place.place.entity.Place;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PlaceResDto {
+    private Long id;
+    private String cityName;
+    private String placeName;
+    private String description;
+    private Category category;
+
+    public PlaceResDto(Place place) {
+        this.id = place.getId();
+        this.cityName = place.getCityName();
+        this.placeName = place.getPlaceName();
+        this.description = place.getDescription();
+        this.category = place.getCategory();
+    }
+}
