c0c8c363518fb5b635993bc12208bb445c4d32f9
ShinYounHo <suh2963@gmail.com>
2025-03-05 15:16:16 +0900

    ✨ Feat: 비밀번호 암호화 및 이메일 인증 구현

--- backend/src/main/java/com/tripfriend/TravelPlanApplication.java
+++ backend/src/main/java/com/tripfriend/TravelPlanApplication.java
@@ -3,13 +3,14 @@ package com.tripfriend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class TravelPlanApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		SpringApplication.run(TravelPlanApplication.class, args);
 	}
 
