a4df43f65afa002052a8fd2618ffea4bd1765a53
hoechanj <154239980+hoechanj@users.noreply.github.com>
2025-03-05 14:44:07 +0900

    ✨feat(20): notice crud 작성 (#31)
    * :sparkles: feat: 회원 entity 설정
    
    * ✨ Feat: 시큐리티 설정
    
    * 🗑️ Remove: example 패키지 및 하위 파일 삭제
    
    * feat(3): 후기 게시판 entity 설정 (#13)
    
    * ✨ feat: 리 entity 추가
    
    * ✨ feat:댓글 entity 추가
    
    * ✨feat(16): notice entity 작성 (#18)
    
    * 🔧chore : 빌드 문제 수정
    
    - com.example.travelPlan에서 com.tripfriend로 수정
    
    * [feat]notice entity 작성
    
    ---------
    
    Co-authored-by: GODAOS <jmskkr@gmail.com>
    
    * ✨ Feat: 회원 CRUD 기능 구현 (#19)
    
    * ✨[feat]:notice repository 작성
    
    * ♻️[refactoring]:notice entity 디렉토리 생성
    
    * ✨[feat]:공지사항 저장/삭제 구현
    
    * ✨[feat]:공지사항 조회/수정 구현
    
    * ✨[feat]:공지 컨트롤러 생성/조회 구현
    
    * ✨[feat]:공지 컨트롤러 수정/삭제 구현
    
    ---------
    
    Co-authored-by: ShinYounHo <suh2963@gmail.com>
    Co-authored-by: Hyunmo Park <39055629+Emokido@users.noreply.github.com>
    Co-authored-by: GODAOS <jmskkr@gmail.com>
    Co-authored-by: Shin Youn Ho <115200471+messiteacher@users.noreply.github.com>

---
+++ backend/src/main/java/com/tripfriend/domain/notice/controller/NoticeController.java
@@ -0,0 +1,49 @@
+package com.tripfriend.domain.notice.controller;
+
+import com.tripfriend.domain.notice.entity.Notice;
+import com.tripfriend.domain.notice.repository.NoticeRepository;
+import com.tripfriend.domain.notice.service.NoticeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/notice")
+@RequiredArgsConstructor
+public class NoticeController {
+    private final NoticeRepository noticeRepository;
+    private final NoticeService noticeService;
+
+    //공지사항 생성
+    @PostMapping
+    public ResponseEntity<Notice> createNotice(@RequestBody Notice notice) {
+        return ResponseEntity.ok(noticeService.createNotice(notice.getTitle(), notice.getContent()));
+    }
+
+    //공지사항 전체 조회
+    @GetMapping
+    public ResponseEntity<List<Notice>> getAllNotices() {
+        return ResponseEntity.ok(noticeRepository.findAll());
+    }
+
+    // 공지사항 검색 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<Notice> getNoticeById(@PathVariable Long id) {
+        return ResponseEntity.ok(noticeService.getNoticeById(id));
+    }
+
+    //공지사항 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<Notice> updateNotice(@PathVariable Long id, @RequestBody Notice notice) {
+        return ResponseEntity.ok(noticeService.updateNoticeById(id, notice.getTitle(), notice.getContent()));
+    }
+
+    //공지사항 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNotice(@PathVariable Long id) {
+        noticeService.deleteNotice(id);
+        return ResponseEntity.noContent().build();
+    }
+}
