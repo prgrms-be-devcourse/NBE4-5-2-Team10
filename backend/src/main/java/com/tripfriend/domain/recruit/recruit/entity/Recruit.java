fdd43b673a7562522f8963eecd090d8c0112f75c
Hweepadam <160405935+huipadyam@users.noreply.github.com>
2025-03-06 14:43:15 +0900

    feat(26): 동행 모집 & 동행 요청 CRUD 기능 추가 (#43)
    * :sparkles: feat: 동행모집 생성, 단건조회 기능 추가
    
    * :sparkles: feat: 동행모집 생성, 단건조회 기능 추가
    
    * :sparkles: feat: 동행모집 목록 조회 기능 추가
    
    * :sparkles: feat: 동행모집 수정 기능 추가
    
    * :sparkles: feat: 동행모집 삭제 기능 추가
    
    * :sparkles: feat: 동행요청 등록, 목록조회 기능 추가
    
    * :sparkles: feat: 동행요청 삭제 기능 추가
    
    * :bug: fix: columndefinition text 추가

--- backend/src/main/java/com/tripfriend/domain/recruit/recruit/entity/Recruit.java
+++ backend/src/main/java/com/tripfriend/domain/recruit/recruit/entity/Recruit.java
@@ -1,7 +1,9 @@
 package com.tripfriend.domain.recruit.recruit.entity;
 
 import com.tripfriend.domain.member.member.entity.Member;
+import com.tripfriend.domain.place.place.entity.Place;
 import com.tripfriend.domain.recruit.apply.entity.Apply;
+import com.tripfriend.domain.recruit.recruit.dto.RecruitUpdateRequestDto;
 import com.tripfriend.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -14,31 +16,32 @@ import java.util.List;
 @Table(name = "Recruit")
 @Getter
 // @ToString
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // jpa가 엔티티 생성할 때 필요로 함
+@AllArgsConstructor // builder에 필요함
 @Builder
 public class Recruit extends BaseEntity {
     @Id
     @Column(name = "recruit_id") // 기본키는 원래 not null
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
     private Long recruitId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = true) // 컬럼이름?, 회원 탈퇴 시 게시글 남아있게 함
-    private Member member;
-
-    @OneToMany(mappedBy = "recruit", cascade = CascadeType.ALL, orphanRemoval = true)
-    // 글 삭제 시 댓글도 삭제, 리스트에서 제거된 댓글 자동 삭제
-    private List<Apply> applies = new ArrayList<>();
-
 //    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "place_id", nullable = false) // 컬럼이름?? 장소 선택 필수?
+//    @JoinColumn(name = "member_id") // 컬럼이름?, 회원 탈퇴 시 게시글 남아있게 함
+//    private Member member;
+
+    @OneToMany(mappedBy = "recruit", cascade = CascadeType.REMOVE) // , orphanRemoval = true
+    @OrderBy("applyId asc")
+    // 글 삭제 시 댓글도 삭제, 리스트에서 제거된 댓글 자동 삭제
+    private List<Apply> applies;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "placeId", nullable = false) // 컬럼이름?? 장소 선택 필수?
 //    private Place place;
 
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(name = "is_closed", nullable = false)
@@ -65,4 +68,18 @@ public class Recruit extends BaseEntity {
 
     @Column(name = "group_size", nullable = false)
     private Integer groupSize = 2;
+
+    public Recruit update(RecruitUpdateRequestDto recruitUpdateRequestDto){
+        this.title = recruitUpdateRequestDto.getTitle();
+        this.content = recruitUpdateRequestDto.getContent();
+        this.isClosed = recruitUpdateRequestDto.isClosed();
+        this.startDate = recruitUpdateRequestDto.getStartDate();
+        this.endDate = recruitUpdateRequestDto.getEndDate();
+        this.travelStyle = recruitUpdateRequestDto.getTravelStyle();
+        this.sameGender = recruitUpdateRequestDto.isSameGender();
+        this.sameAge = recruitUpdateRequestDto.isSameAge();
+        this.budget = recruitUpdateRequestDto.getBudget();
+        this.groupSize = recruitUpdateRequestDto.getGroupSize();
+        return this;
+    }
 }
---
+++ backend/src/main/java/com/tripfriend/domain/recruit/recruit/service/RecruitService.java
@@ -0,0 +1,51 @@
+package com.tripfriend.domain.recruit.recruit.service;
+
+import com.tripfriend.domain.recruit.recruit.dto.RecruitCreateRequestDto;
+import com.tripfriend.domain.recruit.recruit.dto.RecruitDetailResponseDto;
+import com.tripfriend.domain.recruit.recruit.dto.RecruitListResponseDto;
+import com.tripfriend.domain.recruit.recruit.dto.RecruitUpdateRequestDto;
+import com.tripfriend.domain.recruit.recruit.entity.Recruit;
+import com.tripfriend.domain.recruit.recruit.repository.RecruitRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RecruitService {
+    private final RecruitRepository recruitRepository;
+
+    @Transactional
+    public RecruitDetailResponseDto findById(Long id) {
+        Recruit recruit = recruitRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("recruit not foud with id : " + id));
+        return new RecruitDetailResponseDto(recruit);
+    }
+
+    @Transactional
+    public RecruitDetailResponseDto create(RecruitCreateRequestDto recruitCreateRequestDto) {
+        return new RecruitDetailResponseDto(recruitRepository.save(recruitCreateRequestDto.toEntity()));
+    }
+
+    @Transactional
+    public List<RecruitListResponseDto> findAll() {
+        return recruitRepository.findAll().stream()
+                .map(RecruitListResponseDto::new)
+                .toList();
+    }
+
+    @Transactional
+    public RecruitDetailResponseDto update(RecruitUpdateRequestDto recruitUpdateRequestDto) {
+        Long recruitId = recruitUpdateRequestDto.getRecruitId();
+        Recruit recruit = recruitRepository.findById(recruitId).orElseThrow(() -> new EntityNotFoundException("recruit not foud with id : " + recruitId));
+        recruit.update(recruitUpdateRequestDto);
+        return new RecruitDetailResponseDto(recruit); // recruitRepository.save(recruit) 불필요!
+    }
+
+    public void delete(Long recruitId) {
+        recruitRepository.deleteById(recruitId);
+    }
+}
