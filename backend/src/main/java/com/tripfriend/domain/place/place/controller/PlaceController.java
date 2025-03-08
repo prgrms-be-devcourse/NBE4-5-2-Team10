610132faf4b43f721900386776981fb990fa1c9f
GODAOS <jmskkr@gmail.com>
2025-03-08 17:20:55 +0900

    ♻️refactor : Swagger 설명 추가
    - Tag : name, description 추가
    - 여행지 API Operation 추가

--- backend/src/main/java/com/tripfriend/domain/place/place/controller/PlaceController.java
+++ backend/src/main/java/com/tripfriend/domain/place/place/controller/PlaceController.java
@@ -5,6 +5,8 @@ import com.tripfriend.domain.place.place.dto.PlaceResDto;
 import com.tripfriend.domain.place.place.dto.PlaceUpdateReqDto;
 import com.tripfriend.domain.place.place.entity.Place;
 import com.tripfriend.domain.place.place.service.PlaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,25 +16,29 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/place")
+@Tag(name = "Place API", description = "여행지 관련 기능을 제공합니다.")
 public class PlaceController {
 
     private final PlaceService placeService;
 
-    // 여행 장소 등록
+    // 여행지 등록
     @PostMapping
+    @Operation(summary = "여행지 등록")
     public ResponseEntity<Place> createPlace(@RequestBody PlaceCreateReqDto placeCreateRequestDto) {
         Place savePlace = placeService.createPlace(placeCreateRequestDto);
         return ResponseEntity.ok(savePlace);
     }
 
-    // 모든 여행 장소 조회
+    // 전체 여행지 조회
     @GetMapping
+    @Operation(summary = "전체 여행지 조회")
     public ResponseEntity<List<Place>> getAllPlaces() {
         return ResponseEntity.ok(placeService.getAllPlaces());
     }
 
-    // 여행 장소 - 단건 조회
+    // 여행지 조회 - 단건 조회
     @GetMapping("/{id}")
+    @Operation(summary = "여행지 조회")
     public ResponseEntity<Place> getPlace(@PathVariable Long id) {
         Place place = placeService.getPlace(id).orElseThrow(
                 () -> new RuntimeException("존재하지 않는 장소입니다.")
@@ -41,8 +47,9 @@ public class PlaceController {
         return ResponseEntity.ok(place);
     }
 
-    // 여행 장소 삭제
+    // 여행지 삭제
     @DeleteMapping("/{id}")
+    @Operation(summary = "여행지 삭제")
     public ResponseEntity<Void> deletePlace(@PathVariable Long id) {
 
         Place place = placeService.getPlace(id).orElseThrow(
@@ -53,8 +60,9 @@ public class PlaceController {
         return ResponseEntity.noContent().build();
     }
 
-    // 여행 장소 수정
+    // 여행지 정보 수정
     @PutMapping("/{id}")
+    @Operation(summary = "여행지 정보 수정")
     public ResponseEntity<PlaceResDto> updatePlace(@PathVariable Long id, @RequestBody PlaceUpdateReqDto placeUpdateReqDto) {
         Place place = placeService.getPlace(id).orElseThrow(
                 () -> new RuntimeException("존재하지 않는 장소입니다.")
