13a70ba7d058300cb2606adb2c832f626264633c
GODAOS <jmskkr@gmail.com>
2025-03-13 18:25:28 +0900

    ✨feat : 여행 정보 추가 메소드 구현
    - 세부일정 등록 구현
    - 세부일정 삭제 구현
    - 방문일정 변경 구현

--- backend/src/main/java/com/tripfriend/domain/trip/information/controller/TripInformationController.java
+++ backend/src/main/java/com/tripfriend/domain/trip/information/controller/TripInformationController.java
@@ -1,7 +1,9 @@
 package com.tripfriend.domain.trip.information.controller;
 
+import com.tripfriend.domain.trip.information.dto.TripInformationReqDto;
 import com.tripfriend.domain.trip.information.dto.TripInformationResDto;
 import com.tripfriend.domain.trip.information.dto.TripInformationUpdateReqDto;
+import com.tripfriend.domain.trip.information.dto.VisitedReqDto;
 import com.tripfriend.domain.trip.information.entity.TripInformation;
 import com.tripfriend.domain.trip.information.service.TripInformationService;
 import com.tripfriend.global.dto.RsData;
@@ -34,6 +36,43 @@ public class TripInformationController {
         );
     }
 
-    //
+    // 세부 일정 등록
+    @PostMapping
+    public RsData<TripInformationResDto> createTripInformation(
+            @Valid @RequestBody TripInformationReqDto reqDto,
+            @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "").trim();
+        TripInformationResDto resDto = tripInformationService.addTripInformation(reqDto, token);
+        return new RsData<>(
+                "200",
+                "세부 일정 등록 성공",
+                resDto
+        );
+    }
+
+    // 세부 일정 삭제
+    @DeleteMapping("/{tripInformationId}")
+    public RsData<Void> deleteTripInformation(
+            @PathVariable Long tripInformationId,
+            @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "").trim();
+        tripInformationService.deleteTripInformation(tripInformationId, token);
+        return new RsData<>(
+                "200",
+                "세부 일정 삭제 성공"
+        );
+    }
+
+    // 방문 여부 변경
+    @PutMapping("/update-visited")
+    public RsData<Void> updateVisited(
+            @Valid @RequestBody VisitedReqDto reqDto,
+            @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "").trim();
+        tripInformationService.updateVisited(reqDto, token);
+        return new RsData<>("200",
+                "방문 여부 업데이트 성공"
+        );
+    }
 
 }
