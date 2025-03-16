2318a22f7a524d535b6554e6ebcffb72ebb0f7b4
GODAOS <jmskkr@gmail.com>
2025-03-16 16:43:21 +0900

    ♻️refactor : 이미지 Url 이미지가 없을 경우 로직 추가

--- backend/src/main/java/com/tripfriend/domain/place/place/service/PlaceService.java
+++ backend/src/main/java/com/tripfriend/domain/place/place/service/PlaceService.java
@@ -114,13 +114,11 @@ public class PlaceService {
     public String uploadPlaceImage(MultipartFile imageFile) throws IOException {
 
         String imageUrl = imageUtil.saveImage(imageFile);
-//        if (imageUrl != null) {
-//            throw new ServiceException("404-2", "이미지 업로드에 실패하였습니다.");
-//        }
+        if (imageUrl == null) {
+            throw new ServiceException("404-2", "이미지 업로드에 실패하였습니다.");
+        }
 
         return imageUrl;
     }
-
-    // 여행 이미지 삭제
 
 }
