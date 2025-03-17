ef6208b8f072f7c4ea6e345eb31079953d732ac3
kyungrae kim <15260002+GODAOS@users.noreply.github.com>
2025-03-17 14:43:17 +0900

    📚docs : Primary 어노테이션 제거

--- backend/src/main/java/com/tripfriend/global/config/RedisConfig.java
+++ backend/src/main/java/com/tripfriend/global/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.tripfriend.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
